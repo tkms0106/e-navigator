e35db9a0c0ccc2de0452bbd8fb0d37ddb6785cb7
tkms0106 <23391543+tkms0106@users.noreply.github.com>
2018-03-26 23:29:41 +0900

    [add]rails g controller Pages index

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
-  get 'users/edit'
-
-  get 'users/update'
+  get 'pages/index'
 
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users, only: [:edit, :update]
 end
