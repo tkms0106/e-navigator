fe7d1458914864fb0b1bdc74c5858fee36a6c81f
tkms0106 <siberianexpress1991@gmail.com>
2018-03-26 23:30:33 +0900

    [update]rootの指定

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
-  get 'pages/index'
-
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users, only: [:edit, :update]
+
+  devise_scope :user do
+    authenticated :user do
+      root 'pages#index', as: :authenticated_root
+    end
+    unauthenticated do
+      root 'devise/sessions#new', as: :unauthenticated_root
+    end
+  end
 end
