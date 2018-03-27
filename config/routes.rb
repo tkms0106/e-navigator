e71e3b8054a5436c8e8ce2a3c05b676f9ff44e94
tkms0106 <23391543+tkms0106@users.noreply.github.com>
2018-03-27 22:35:23 +0900

    [add]Usersコントローラにアクションを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :users, only: [:edit, :update]
+  resources :users, only: [:index, :show, :edit, :update]
 
   devise_scope :user do
     authenticated :user do
-      root 'pages#index', as: :authenticated_root
+      root 'users#index', as: :authenticated_root
     end
     unauthenticated do
       root 'devise/sessions#new', as: :unauthenticated_root
