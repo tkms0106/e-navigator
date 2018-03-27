417b8ab27d63dcedb83c5c419ee9fb190f8ffbde
tkms0106 <23391543+tkms0106@users.noreply.github.com>
2018-03-27 23:13:45 +0900

    [remove]Usersコントローラのshowアクションを削除

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :users, only: [:index, :show, :edit, :update]
+  resources :users, only: [:index, :edit, :update]
 
   devise_scope :user do
     authenticated :user do
