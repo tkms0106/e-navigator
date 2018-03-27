6dba25052e1fe96e232c70c0172cd7a5d15e75e2
tkms0106 <siberianexpress1991@gmail.com>
2018-03-27 23:42:57 +0900

    [add]プロフィール編集機能を追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,8 +4,17 @@ class UsersController < ApplicationController
   end
 
   def edit
+    @user = current_user
   end
 
   def update
+    current_user.update_attributes(user_params)
+    redirect_to users_path
   end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :date_of_birth, :gender, :school)
+    end
 end
