722591f6a955d3e5c369838daf3ba689b39b88bb
tkms0106 <23391543+tkms0106@users.noreply.github.com>
2018-03-28 23:26:16 +0900

    [update]生年月日を明日以降に設定できないように変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,12 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   enum gender: [:male, :female, :other]
+  before_validation :calc_age
+  validates :age, numericality: { greater_than_or_equal_to: 0 }
+
+  private
+    def calc_age
+      date_format = "%Y%m%d"
+      self.age = (Date.today.strftime(date_format).to_i - date_of_birth.strftime(date_format).to_i) / 10000
+    end
 end
