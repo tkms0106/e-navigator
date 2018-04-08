2a7f2c85481da6b0b20507da6914bb1d7a5964a2
tkms0106 <23391543+tkms0106@users.noreply.github.com>
2018-04-08 20:36:43 +0900

    [update]rails db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180328135509) do
+ActiveRecord::Schema.define(version: 20180408113537) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "interviews", force: :cascade do |t|
+    t.datetime "scheduled_at"
+    t.boolean "is_available"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
