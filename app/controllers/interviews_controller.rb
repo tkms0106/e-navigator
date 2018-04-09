d1cd3fe625d36b53154bcd2ec3fa980d005b0d68
tkms0106 <siberianexpress1991@gmail.com>
2018-04-10 07:21:15 +0900

    [add]面接作成機能を追加

--- app/controllers/interviews_controller.rb
+++ app/controllers/interviews_controller.rb
@@ -8,5 +8,18 @@ class InterviewsController < ApplicationController
   end
 
   def create
+    @interview = current_user.interviews.build(interview_params)
+    if @interview.save
+      flash[:notice] = 'Suscessfully created.'
+    else
+      flash[:alert] = 'Failed to create a interview.'
+    end
+    redirect_to interviews_path
   end
+
+  private
+
+    def interview_params
+      params.require(:interview).permit(:scheduled_at)
+    end
 end
