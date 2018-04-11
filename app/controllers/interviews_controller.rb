e94bb8e6798ee76e1aadf6695cb32bf651fb3192
tkms0106 <23391543+tkms0106@users.noreply.github.com>
2018-04-11 23:05:23 +0900

    [clean]重複コードの削除

--- app/controllers/interviews_controller.rb
+++ app/controllers/interviews_controller.rb
@@ -1,16 +1,15 @@
 class InterviewsController < ApplicationController
+  before_action :set_user
+
   def index
-    @user = set_user
     @interviews = @user.interviews
   end
 
   def new
-    @user = set_user
     @interview = Interview.new
   end
 
   def create
-    @user = set_user
     @interview = @user.interviews.build(interview_params)
     if @interview.save
       flash[:notice] = 'Suscessfully created.'
@@ -23,7 +22,7 @@ class InterviewsController < ApplicationController
   private
 
     def set_user
-      user = User.find_by(id: params[:user_id])
+      @user = User.find_by(id: params[:user_id])
     end
 
     def interview_params
