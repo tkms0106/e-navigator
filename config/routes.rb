a488a2118b9b3552990e2719f84c9af663de28aa
tkms0106 <siberianexpress1991@gmail.com>
2018-04-16 20:11:43 +0900

    [change]rootのルーティングをコントローラ直接アクセスからリダイレクトに変更

--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,10 @@ Rails.application.routes.draw do
   devise_for :users
   devise_scope :user do
     authenticated :user do
-      root 'users#index', as: :authenticated_root
+      root to: redirect('/users/'), as: :authenticated_root
     end
     unauthenticated do
-      root 'devise/sessions#new', as: :unauthenticated_root
+      root to: redirect('/users/sign_in'), as: :unauthenticated_root
     end
   end
 end
