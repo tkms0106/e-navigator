f94963b1d5039fc4b7d2eda9152e8694885bc70d
tkms0106 <23391543+tkms0106@users.noreply.github.com>
2018-04-19 22:24:53 +0900

    [change]年齢のバリデーションを廃止

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,17 +4,13 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   enum gender: { not_known: 0, male: 1, female: 2, not_applicable: 9 }
-  before_validation :calc_age
-  validates :age, numericality: { greater_than_or_equal_to: 0 }
-  has_many :interviews
+  before_save :calc_age
+  has_many :interviews, dependent: :delete_all
 
   private
     def calc_age
-      unless self.date_of_birth.nil?
-        date_format = "%Y%m%d"
-        self.age = (Date.today.strftime(date_format).to_i - self.date_of_birth.strftime(date_format).to_i) / 10000
-      else
-        self.age = 0
-      end
+      return if self.date_of_birth.nil?
+      date_format = "%Y%m%d"
+      self.age = (Date.today.strftime(date_format).to_i - self.date_of_birth.strftime(date_format).to_i) / 10000
     end
 end
