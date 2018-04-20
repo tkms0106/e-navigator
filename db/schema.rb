b947f090e74f62530ebc9068ac9167a546c6559a
tkms0106 <23391543+tkms0106@users.noreply.github.com>
2018-04-20 20:02:18 +0900

    [remove]is_availableカラムを削除

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180408123033) do
+ActiveRecord::Schema.define(version: 20180420110004) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "interviews", force: :cascade do |t|
     t.datetime "scheduled_at"
-    t.boolean "is_available"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id"
