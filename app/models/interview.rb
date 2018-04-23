e05b1317341e42dbb740ac51e098b50adcdf3d5b
tkms0106 <siberianexpress1991@gmail.com>
2018-04-23 21:13:19 +0900

    [update]is_availableカラムをavailabilityへ改名

--- app/models/interview.rb
+++ app/models/interview.rb
@@ -1,6 +1,6 @@
 class Interview < ApplicationRecord
-  enum is_available: { pending: 0, approval: 1, disapproval: 2 }
+  enum availability: { pending: 0, approval: 1, disapproval: 2 }
   validates :scheduled_at, presence: true
-  validates :is_available, presence: true
+  validates :availability, presence: true
   belongs_to :user
 end
