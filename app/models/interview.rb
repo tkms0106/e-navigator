7fca3c38c70f5cbd1ca9caf8defa794a0b9a394f
tkms0106 <23391543+tkms0106@users.noreply.github.com>
2018-04-23 21:09:14 +0900

    [update]enumのメンバ名を変更

--- app/models/interview.rb
+++ app/models/interview.rb
@@ -1,5 +1,5 @@
 class Interview < ApplicationRecord
-  enum is_available: { to_be_determined: 0, approval: 1, disapproval: 2 }
+  enum is_available: { pending: 0, approval: 1, disapproval: 2 }
   validates :scheduled_at, presence: true
   validates :is_available, presence: true
   belongs_to :user
