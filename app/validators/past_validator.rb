d066230da37d1fe0457757070f974e0017c1932c
tkms0106 <siberianexpress1991@gmail.com>
2018-05-26 15:10:23 +0900

    [fix]エラーメッセージの修正

--- app/validators/past_validator.rb
+++ app/validators/past_validator.rb
@@ -1,7 +1,7 @@
 class PastValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
     unless value.past?
-      record.errors[attribute] << (options[:message] || "は未来ではありません")
+      record.errors[attribute] << (options[:message] || "は過去ではありません")
     end
   end
 end
