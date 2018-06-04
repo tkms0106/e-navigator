e28be2fcd51298c52c8d16c59c73cc24ee01ed8d
tkms0106 <siberianexpress1991@gmail.com>
2018-06-04 22:33:48 +0900

    [add]rails g mailer UserMailer approval_request approval_notification

---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,24 @@
+class UserMailer < ApplicationMailer
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.user_mailer.approval_request.subject
+  #
+  def approval_request
+    @greeting = "Hi"
+
+    mail to: "to@example.org"
+  end
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.user_mailer.approval_notification.subject
+  #
+  def approval_notification
+    @greeting = "Hi"
+
+    mail to: "to@example.org"
+  end
+end
